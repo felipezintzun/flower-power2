94bea5dab42b88da4807a7175bad08559fd9bc15
felipezintzun <felipezintzun@gmail.com>
2022-04-21 00:15:04 -0500

    first commit

---
+++ client/src/data.js
@@ -0,0 +1,65 @@
+export const gallery = [
+    {
+        id: 1,
+        img: "https://github.com/felipezintzun/flower-power/blob/feature/flower-post/client/src/img/orchid-flower.jpg?raw=true",
+        link: "",
+        name: "Orchid",
+        description: "Orchids are beatiful...",
+    },
+    {
+        id: 2,
+        img: "https://github.com/felipezintzun/flower-power/blob/feature/flower-thoughts/client/src/img/bougainvillea-flower.jpg?raw=true",
+        link: "",
+        name: "Bougainvillea",
+        description: "These flowers are the best...",
+    },
+    {
+        id: 3,
+        img: "https://github.com/felipezintzun/flower-power/blob/feature/flower-thoughts/client/src/img/rosemary.jpg?raw=true",
+        name: "Rosemary",
+        description: "Rosemary smells like Christmas..",
+    },
+    {
+        id: 4,
+        img: "https://github.com/felipezintzun/flower-power/blob/feature/flower-thoughts/client/src/img/jade-plant.jpg?raw=true",
+        name: "Jade",
+        description: "Jade plants are super easy...",
+    },
+    {
+        id: 5,
+        img: "https://github.com/felipezintzun/flower-power/blob/feature/flower-thoughts/client/src/img/lavender.jpg?raw=true",
+        name: "Lavender",
+        description: "Lavender, sometimes considered...",
+    },
+    {
+        id: 6,
+        img: "https://github.com/felipezintzun/flower-power/blob/feature/flower-thoughts/client/src/img/clotilde-soupert-antique-rose.jpg?raw=true",
+        name: "Clotilde Soupert",
+        description: "This beatiful rose...",
+    },
+    {
+        id: 7,
+        img: "https://github.com/felipezintzun/flower-power/blob/feature/flower-thoughts/client/src/img/buttercup-flower.jpg?raw=true",
+        name: "Buttercup",
+        description: "This small flower brings...",
+    },
+    {
+        id: 8,
+        img: "https://github.com/felipezintzun/flower-power/blob/feature/flower-thoughts/client/src/img/thyme.jpg?raw=true",
+        name: "Thyme",
+        description: "Thyme is great for cooking...",
+    },
+    {
+        id: 9,
+        img: "https://github.com/felipezintzun/flower-power/blob/feature/flower-thoughts/client/src/img/philodendron-plant.jpg?raw=true",
+        name: "Philodendron",
+        description: "This plant is a very bright...",
+    },
+    {
+        id: 10,
+        img: "https://github.com/felipezintzun/flower-power/blob/feature/flower-thoughts/client/src/img/mouse-ear-flower.jpg?raw=true",
+        name: "Coreopsis, Mouse-Ear",
+        description: "My favorite of the...",
+    },
+    
+    ];
